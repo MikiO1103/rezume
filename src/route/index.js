ed35a8376ac6e2262eab51689152534c0c1f040d
MikhaylO <mikhayl.oleksishin@gmail.com>
2023-04-09 17:29:25 +0300

    done 4 lessons

--- src/route/index.js
+++ src/route/index.js
@@ -5,6 +5,35 @@ const router = express.Router()
 
 // ================================================================
 
+var header = {
+  name: {
+    firstname: 'Ivan',
+    lastname: 'Ivanov',
+  },
+  position: 'Junior Fullstack JS Developer',
+  salary: '600$ в місяц',
+  adress: 'Ukraine, Kyiv',
+}
+
+var footer = {
+  social: {
+    email: {
+      href: 'mailto:ivanov@mail.com',
+      text: 'ivanov@mail.com',
+    },
+    phone: {
+      text: '+380670000123',
+      href: 'tel:+380670000123',
+    },
+    linkedin: {
+      text: 'LinkedIn',
+      href: 'https://www.linkedin.com/in/dmytro-test',
+    },
+  },
+}
+
+// ================================================================
+
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
@@ -23,9 +52,230 @@ router.get('/summary', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
     // ↙ сюди вводимо JSON дані
+
+    page: {
+      title: 'Resume | Summary',
+    },
+
+    header,
+
+    main: {
+      summary: {
+        title: 'Summary',
+        text: `Open-minded for new technologies, with 1 years of experience in development. Whenever I start to work on a new project I learn the domain and try to understand the idea of the project. Good team player, every colleague is a friend to me.`,
+      },
+
+      experience: {
+        title: 'Other experience',
+        text: `Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics (tournament position, goals etc), analyzing by simple mathematics models and preparing probability for such events like: money line - first win / draw / second win, totals etc.`,
+      },
+    },
+
+    footer,
   })
 })
 
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/skills', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('skills', {
+    // ↙ сюди вводимо JSON дані
+
+    page: {
+      title: 'Resume | Skills',
+    },
+
+    header,
+
+    main: {
+      skills: [
+        {
+          name: 'HTML',
+          point: 10,
+          isTop: true,
+        },
+
+        {
+          name: 'Handlebars',
+          point: 10,
+          isTop: true,
+        },
+
+        {
+          name: 'VS Code',
+          point: 10,
+          isTop: true,
+        },
+
+        {
+          name: 'Git',
+          point: 10,
+          isTop: false,
+        },
+
+        {
+          name: 'Terminal',
+          point: 10,
+          isTop: false,
+        },
+
+        {
+          name: 'ReactJS',
+          point: 0,
+          isTop: 0,
+        },
+
+        {
+          name: 'PHP',
+          point: null,
+        },
+      ],
+
+      hobbies: [
+        {
+          name: 'Hobbies 1',
+          isMain: true,
+        },
+        {
+          name: 'Hobbies 2',
+          isMain: false,
+        },
+        {
+          name: 'Hobbies 3',
+          isMain: true,
+        },
+      ],
+    },
+
+    footer,
+  })
+})
+
+router.get('/education', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('education', {
+    page: {
+      title: 'Resume | Educations',
+    },
+
+    header,
+
+    main: {
+      educations: [
+        {
+          name: 'Univer1',
+          isEnd: true,
+        },
+        {
+          name: 'Univer2',
+          isEnd: true,
+        },
+        {
+          name: 'Kurs1',
+          isEnd: true,
+        },
+        {
+          name: 'Kurs2',
+          isEnd: false,
+        },
+        {
+          name: 'Kurs3',
+          isEnd: false,
+        },
+      ],
+
+      certificates: [
+        {
+          name: 'Certificate1',
+          id: 62517621,
+        },
+        {
+          name: 'Certificate2',
+          id: 21368378,
+        },
+        {
+          name: 'Certificate3',
+          id: 76527157,
+        },
+      ],
+
+      summary: {
+        title: 'Summary',
+        text: `Open-minded for new technologies, with 1 years of experience in development. Whenever I start to work on a new project I learn the domain and try to understand the idea of the project. Good team player, every colleague is a friend to me.`,
+      },
+
+      experience: {
+        title: 'Other experience',
+        text: `Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics (tournament position, goals etc), analyzing by simple mathematics models and preparing probability for such events like: money line - first win / draw / second win, totals etc.`,
+      },
+    },
+
+    footer,
+  })
+})
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/work', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('work', {
+    // ↙ сюди вводимо JSON дані
+
+    layout: 'big',
+
+    page: {
+      title: 'Resume | Work',
+    },
+
+    header,
+
+    main: {
+      works: [
+        {
+          position: 'Junior Fullstack Developer',
+          company: {
+            name: 'IT Brains',
+            url: 'https://it-brains.com.ua',
+          },
+          duration: {
+            from: '10.10.2021',
+            to: null,
+          },
+          projectAmount: 3,
+          projects: [
+            {
+              name: 'Resume',
+              url: 'https://resume.com.ua',
+              about: 'Arbeit heute und morgen',
+              stacks: [
+                {
+                  name: 'React.JS',
+                },
+                {
+                  name: 'HTML / CSS',
+                },
+                {
+                  name: 'Node.JS',
+                },
+              ],
+              awards: [
+                {
+                  name: 'Meine Errungenschaften',
+                },
+                {
+                  name: 'Meine Errungenschaften Zwei',
+                },
+              ],
+              stackAmount: 12,
+              awardAmount: 10,
+            },
+          ],
+        },
+      ],
+    },
+
+    footer,
+  })
+})
 // ================================================================
 
 // Підключаємо роутер до бек-енду
